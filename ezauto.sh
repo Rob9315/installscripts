56933c4d7eef857f1109fbe2cbbfabcf66b2f7ab
Rob9315 <dev.robk@gmail.com>
2021-03-29 00:22:18 +0200

    Create ezauto.sh

---
+++ ezauto.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+read -p "Please enter the installscript name (without .sh): " distroname
+read -n 1 -p "Do you want to use the program defaults? (y|N)" yn
+case $yn in
+  [Yy]*) yn="1";;
+  *) yn="0";;
+esac
+if [ $yn -eq 1 ]
+then
+  sh -c "$(curl "https://raw.githubusercontent.com/Rob9315/installscripts/master/defaults.sh")"
+fi
+sh -c "$(curl "https://raw.githubusercontent.com/Rob9315/installscripts/master/$distroname.sh")"
