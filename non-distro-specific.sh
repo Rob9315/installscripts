e9368ad07324b33180a167d4bdcb5b8756f69b14
Rob9315 <dev.robk@gmail.com>
2021-03-28 23:24:03 +0200

    Create non-distro-specific.sh

---
+++ non-distro-specific.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# only launch this after you have read what it does or have it launch from a different script
+
+prechosen_or_prompted_command() {
+  while [ -z "${!1}" ]
+  do
+    read -p "Do you want to install '$2'?$4" yn
+    case $yn in
+      [Yy]*) declare "$1"="1";;
+      [Nn]*) declare "$1"="0";;
+      *) echo "Please answer yes or no.";;
+    esac
+  done
+  if [ ${!1} -eq 1 ]
+  then
+    sh -c "$3"
+  fi 
+}
+
+prechosen_or_prompted_command "spotify" "Spotify (adblock)" "curl \"https://raw.githubusercontent.com/Rob9315/installscripts/master/spotify.sh\" | sh"
+# prechosen_or_prompted_command "shell" "Shell" "curl \"https://raw.githubusercontent.com/Rob9315/installscripts/master/shell.sh\" | sh"
+# prechosen_or_prompted_command "git" "Set up Git" "curl \"https://raw.githubusercontent.com/Rob9315/installscripts/master/git.sh\" | sh"
+prechosen_or_prompted_command "gtktheme" "Matcherial Theme" "curl \"https://raw.githubusercontent.com/Rob9315/installscripts/master/gtktheme.sh\" | sh" " (you will need to have sassc installed)"
