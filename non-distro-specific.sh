7a3adab3f8c614d4dc149deb8bb2aa143b26d6b7
Rob9315 <dev.robk@gmail.com>
2021-03-29 22:11:10 +0200

    Update non-distro-specific.sh

--- non-distro-specific.sh
+++ non-distro-specific.sh
@@ -22,7 +22,7 @@ prechosen_or_prompted_command() {
 prechosen_or_prompted_command "spotify" "Spotify (adblock)" "$(curl "https://raw.githubusercontent.com/Rob9315/installscripts/master/spotify.sh")"
 prechosen_or_prompted_command "shell" "Shell" "$(curl "https://raw.githubusercontent.com/Rob9315/installscripts/master/shell.sh")"
 echo -e "shell=$shell"
-if [ -n "$shell" ] ; then 
+if [ "$shell"="1" ] ; then 
   $(read -p "termfont=$termfont")
   export termfont='MesloLGS NF'
   $(read -p "termfont=$termfont")
