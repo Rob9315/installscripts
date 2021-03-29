93c199b592ff1a78ae7a27e7796b17b0f327862e
Rob9315 <dev.robk@gmail.com>
2021-03-29 03:35:10 +0200

    Create shell.sh

---
+++ shell.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo chsh --shell /bin/zsh $USER
+cd /usr/share/fonts
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+fc-cache -f -v
+cd ~
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
