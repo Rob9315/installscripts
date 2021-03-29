c8270e3b55d7965d05224b3c24325af2cfa74ff4
Rob9315 <dev.robk@gmail.com>
2021-03-29 04:06:50 +0200

    Update none.sh

--- none.sh
+++ none.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 # this file exists so one can choose to not apply distro specific patches (again)
+# launch other scripts
+sh -c "$(curl "https://raw.githubusercontent.com/Rob9315/installscripts/master/non-distro-specific.sh")"
