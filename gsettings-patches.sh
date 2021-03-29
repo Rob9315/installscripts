4d9fa6af612488938b254d9f04d8a221a124c2b5
Rob9315 <dev.robk@gmail.com>
2021-03-29 04:03:06 +0200

    Create gsettings-patches.sh

---
+++ gsettings-patches.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# set terminal font if not already set
+if [ -z $termfont ]
+then
+  termfont="Monospace"
+fi
+# get active profile id
+term_active_profile=${$(gsettings get org.gnome.Terminal.ProfilesList default):1:-1}
+
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$term_active_profile/ font "$termfont 10"
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$term_active_profile/ use-system-font false
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$term_active_profile/ use-theme-colors true
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$term_active_profile/ login-shell true
