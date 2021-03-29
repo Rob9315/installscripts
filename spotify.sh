e9b0538d45c8d5b69453a3fd26147b429160cf19
Rob9315 <dev.robk@gmail.com>
2021-03-29 16:54:35 +0200

    Update spotify.sh

--- spotify.sh
+++ spotify.sh
@@ -6,7 +6,7 @@ git clone https://github.com/abba23/spotify-adblock-linux.git
 cd spotify-adblock-linux
 
 #wget new release, untar it
-wget -N -O cef.tar.bz2 https://cef-builds.spotifycdn.com/cef_binary_88.1.6%2Bg4fe33a1%2Bchromium-88.0.4324.96_linux64_minimal.tar.bz2
+wget -nc -O cef.tar.bz2 https://cef-builds.spotifycdn.com/cef_binary_88.1.6%2Bg4fe33a1%2Bchromium-88.0.4324.96_linux64_minimal.tar.bz2
 tar -xf cef.tar.bz2 --wildcards '*include' --strip-components=1
 
 #install
