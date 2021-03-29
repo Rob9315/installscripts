66f5b3010a47d973e39df2c3676e82bc1a54a663
Rob9315 <dev.robk@gmail.com>
2021-03-29 21:25:58 +0200

    Update non-distro-specific.sh

--- non-distro-specific.sh
+++ non-distro-specific.sh
@@ -21,7 +21,9 @@ prechosen_or_prompted_command() {
 
 prechosen_or_prompted_command "spotify" "Spotify (adblock)" "$(curl "https://raw.githubusercontent.com/Rob9315/installscripts/master/spotify.sh")"
 prechosen_or_prompted_command "shell" "Shell" "$(curl "https://raw.githubusercontent.com/Rob9315/installscripts/master/shell.sh")"
-if [ "$shell"="1" ] ; then ; export termfont='MesloLGS NF' ; fi
+if [ "$shell"="1" ] ; then 
+  export termfont='MesloLGS NF' 
+fi
 prechosen_or_prompted_command "gsettings" "Patch some Gnome settings" "$(curl "https://raw.githubusercontent.com/Rob9315/installscripts/master/gsettings-patches.sh")"
 # prechosen_or_prompted_command "git" "Set up Git" "$(curl "https://raw.githubusercontent.com/Rob9315/installscripts/master/git.sh")"
 prechosen_or_prompted_command "gtktheme" "Matcherial Theme" "$(curl "https://raw.githubusercontent.com/Rob9315/installscripts/master/gtktheme.sh")" " (you will need to have sassc installed)"
