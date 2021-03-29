14eb1fdc16f4846bb1ec36d37a2c415b754dcbd6
Rob9315 <dev.robk@gmail.com>
2021-03-29 18:27:14 +0200

    Update gsettings-patches.sh

--- gsettings-patches.sh
+++ gsettings-patches.sh
@@ -10,6 +10,7 @@ term_active_profile=${term_active_profile:1:-1}
 
 # terminal settings
 echo "termfont 10"
+echo -e "$(gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$term_active_profile/ font "$termfont 11")"
 gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$term_active_profile/ font "$termfont 11"
 gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$term_active_profile/ use-system-font false
 gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$term_active_profile/ use-theme-colors true
