60d5e63572ea33e57d6e5a0f310f6b5a15ce8e7c
rob9315 <dev.robk@gmail.com>
2021-03-30 10:50:31 +0200

    change stuff

--- setdefaults.sh
+++ setdefaults.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-# a yes|no prompt
-export promptyn='() {
-  clear
-  [ -z "${!1}" ] && read -n 1 -p "Do you want to install '$2'?$4 (y|N)"$'\n'"> " yn && [ "$yn" = "Y" ] || [ "$yn" = "y" ] && export $1="1" || export $1="0"
-  echo -e ""
-  [ ${!1} -eq 1 ] && sh -c "$3"
-}'
 
 # a input prompt
 # maybe later
--- software.sh
+++ software.sh
@@ -2,6 +2,13 @@
 # only launch this after you have read what it does or have it launch from a different script
 # sh -c "$(curl "https://raw.githubusercontent.com/Rob9315/installscripts/master/software.sh")"
 
+promptyn() {
+  clear
+  [ -z "${!1}" ] && read -n 1 -p "Do you want to install '$2'?$4 (y|N)"$'\n'"> " yn && [ "$yn" = "Y" ] || [ "$yn" = "y" ] && export $1="1" || export $1="0"
+  echo -e ""
+  [ ${!1} -eq 1 ] && sh -c "$3"
+}
+
 [ -z "$tmppath" ] && eval "$(curl -s "https://raw.githubusercontent.com/Rob9315/installscripts/master/setdefaults.sh")"
 
 $promptyn "spotify" "Spotify (adblock)" "$(curl -s "$rawfilesurl/software/spotify.sh")"
