b5e28c96f4370bf3a6d9b40480242e5840d4e0f5
Rob9315 <dev.robk@gmail.com>
2021-03-31 17:22:43 +0200

    Update dotfiles.sh

--- software/dotfiles.sh
+++ software/dotfiles.sh
@@ -6,10 +6,10 @@
 # this uses MY dotfiles. they might not be your preference
 
 # add .cfg to .gitignore
-echo ".cfg" >> .gitignore
+grep .cfg .gitignore || echo ".cfg" >> .gitignore
 
-git clone 'https://github.com/rob9315/dotfiles' $HOME/.cfg
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/.git --work-tree=$HOME'
+git clone 'https://github.com/rob9315/dotfiles' $HOME/.cfg || config pull -f
 [ -e ~/.zshrc ] && echo "alias config='/usr/bin/git --git-dir=/home/\$USER/.cfg/.git/ --work-tree=/home/\$USER'" >> ~/.zshrc
 config config --local status.showUntrackedFiles no
 config checkout
