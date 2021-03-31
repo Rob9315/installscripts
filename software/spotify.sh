d98b93e4330170bbea3b2642ef28f47fe829b2c0
Rob9315 <dev.robk@gmail.com>
2021-03-31 16:26:46 +0200

    Update spotify.sh

--- software/spotify.sh
+++ software/spotify.sh
@@ -3,7 +3,7 @@
 [ -z "$tmppath" ] && eval "$(curl -s "https://raw.githubusercontent.com/Rob9315/installscripts/master/setdefaults.sh")"
 
 #go to tmp, clone adblock and cd into adblock repo
-cd $tmpfolder
+cd $tmppath
 git clone https://github.com/abba23/spotify-adblock-linux.git
 cd spotify-adblock-linux
 
