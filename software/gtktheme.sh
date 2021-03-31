915714ca28e2ee2a43eef4341e013cba759aeed2
Rob9315 <dev.robk@gmail.com>
2021-03-31 16:07:24 +0200

    Update gtktheme.sh

--- software/gtktheme.sh
+++ software/gtktheme.sh
@@ -4,4 +4,5 @@
 
 mkdir -p "$tmppath"
 git -C "$tmppath/" clone https://github.com/rob9315/matcherial-gtk-theme.git || git -C "$tmppath/matcherial-gtk-theme/" pull -f
+cd "$tmppath/matcherial-gtk-theme"
 sh "$tmppath/matcherial-gtk-theme/parse-sass.sh" && sh "$tmppath/matcherial-gtk-theme/install.sh" || (echo "It seems there was a problem, the cache will be cleared. Please try again." && rm -rf "$tmppath/matcherial-gtk-theme")
