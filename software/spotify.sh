c9e76f87583234f5739ff9384966f2ed2d0c9fa7
Rob9315 <dev.robk@gmail.com>
2021-03-31 16:33:17 +0200

    Update spotify.sh

--- software/spotify.sh
+++ software/spotify.sh
@@ -8,7 +8,7 @@ cd $tmppath/spotify-adblock-linux
 
 #wget new release, untar it
 echo -e "wget-ing cef.tar.bz2" && wget -nc -O cef.tar.bz2 https://cef-builds.spotifycdn.com/cef_binary_88.1.6%2Bg4fe33a1%2Bchromium-88.0.4324.96_linux64_minimal.tar.bz2
-[ -e $tmppath/spotify-adblock-linux/include ] && echo -e "cef.tar.bz2 was already un-tar-ed, if adblock doesn't work, delete '$tmppath' and try again" || echo -e "un-tar-ing cef.tar.bz2" && tar -xf cef.tar.bz2 --wildcards '*include' --strip-components=1
+[ -e $tmppath/spotify-adblock-linux/include ] && echo -e "cef.tar.bz2 was already un-tar-ed, if adblock doesn't work, delete '$tmppath' and try again" || echo -e "un-tar-ing cef.tar.bz2" || tar -xf cef.tar.bz2 --wildcards '*include' --strip-components=1
 
 #install
 echo -e "installing spotify-adblock"
