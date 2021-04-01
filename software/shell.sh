e65d55d155836de3b90270b8567668cd46cc7538
Rob9315 <dev.robk@gmail.com>
2021-04-01 17:45:20 +0200

    Update shell.sh

--- software/shell.sh
+++ software/shell.sh
@@ -3,14 +3,16 @@
 [ -z "$tmppath" ] && eval "$(curl -s "https://raw.githubusercontent.com/Rob9315/installscripts/master/setdefaults.sh")"
 
 # just some text
-powlvl10kprezshrctext='# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi' 
-powlvl10kpostzshrctext='# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh'
+powlvl10kprezshrctext='# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc. \
+# Initialization code that may require console input (password prompts, [y/n] \
+# confirmations, etc.) must go above this block; everything else may go below. \
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then \
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" \
+fi \
+' 
+powlvl10kpostzshrctext='# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh. \
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh \
+'
 
 # change default shell
 sudo chsh --shell /bin/zsh $USER
@@ -32,7 +34,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k || git -C ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k pull -f
 # add default powerlevel10k text in front of .zshrc
-grep -Fq "'"$powlvl10kprezshrctext"'" ~/.zshrc || sed -i "1s+^+$powlvl10kprezshrctext\n+" ~/.zshrc
+grep -Fq "'"$powlvl10kprezshrctext"'" ~/.zshrc || sed -i "1s@^@$powlvl10kprezshrctext@g" ~/.zshrc
 # add default powerlevel10k text in the end of .zshrc
 grep -Fq "'"$powlvl10kpostzshrctext"'" ~/.zshrc || echo $powlvl10kpostzshrctext >> ~/.zshrc
 
