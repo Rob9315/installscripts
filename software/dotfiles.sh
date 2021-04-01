ccd6e73404592d3f4d0779e430fea00ed02404ad
Rob9315 <dev.robk@gmail.com>
2021-04-01 19:58:00 +0200

    Update dotfiles.sh

--- software/dotfiles.sh
+++ software/dotfiles.sh
@@ -3,12 +3,14 @@
 [ -z "$tmppath" ] && eval "$(curl -s "https://raw.githubusercontent.com/Rob9315/installscripts/master/setdefaults.sh")"
 
 # DISCLAIMER
-# this uses MY dotfiles. they might not be your preference
+# this uses MY dotfiles by default. they might not be your preference
+
+[ -z "$dotfilesrepo" ] && dotfilesrepo='https://github.com/rob9315/dotfiles'
 
 # add .cfg to .gitignore
 grep -q "\.cfg" .gitignore || echo ".cfg" >> .gitignore
 
-git clone --bare 'https://github.com/rob9315/dotfiles' $HOME/.cfg/.git
+git clone --bare "$dotfilesrepo" $HOME/.cfg/.git
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/.git --work-tree=$HOME'
 config pull -f
 [ -e ~/.zshrc ] && grep -q "source ~/.zsh/aliases" ~/.zshrc || echo "source ~/.zsh/aliases" >> ~/.zshrc
